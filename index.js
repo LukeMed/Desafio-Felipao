bd2347060c229c94ab94d202c91abced3a421aa3
LukeMed <luke.lam1988@gmail.com>
2024-10-31 12:54:47 -0300

    Atualização31/10/2024

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let aluno = "Lucas"
+let aluno = "Lucas A. Medeiros"
 let idade = 17
 let idadePermitida = 18
 
@@ -6,4 +6,4 @@ if(idade >= idadePermitida){
 console.log(aluno + ", você está autorizado.")
 }
 
-console.log(aluno + ", você não está autorizado.")
+console.log(aluno + ", você não está autorizado. Hora de trabalhar no desafio")
